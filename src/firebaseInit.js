2b2afbecf7de1261f1f630528c3ef19dcb473185
AnuragRathore-dev <coderanucoder@gmail.com>
2024-09-03 13:38:37 +0530

    Update firebaseInit.js

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCmn0WUjdKE6CqZ8g17zJJ9G7b7PYchsPc",
+  apiKey: //"your api key", // remove due to security reasons
   authDomain: "photoalbum-8409b.firebaseapp.com",
   projectId: "photoalbum-8409b",
   storageBucket: "photoalbum-8409b.appspot.com",
-  messagingSenderId: "759017016387",
-  appId: "1:759017016387:web:2030feb168d2b8f615d8f6"
+  messagingSenderId: //" messagingSenderId", // remove due to security reasons
+  appId: //"appid" // remove due to security reasons
 };
 
 // Initialize Firebase
