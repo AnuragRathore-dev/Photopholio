5a9b798669ee6df1edd198978b3f8c2d8ae376c3
AnuragRathore-dev <coderanucoder@gmail.com>
2024-10-17 20:29:05 +0530

    Update firebaseInit.js

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: //"your api key", // remove due to security reasons
